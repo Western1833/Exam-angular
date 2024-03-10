aac6eb6224fb1d031f6549359492eb5ef04d528e
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-03-10 04:40:59 +0200

    Added details page + html&css

--- exam-angular/src/app/product/product.module.ts
+++ exam-angular/src/app/product/product.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { CreateCarFormComponent } from './create-car-form/create-car-form.component';
 import { LastThreeProductsComponent } from './last-three-products/last-three-products.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     LastThreeProductsComponent,
     CardComponent,
-    CreateCarFormComponent
+    CreateCarFormComponent,
+    EditComponent,
+    DetailsComponent
   ],
   imports: [
     CommonModule
@@ -16,7 +20,9 @@ import { LastThreeProductsComponent } from './last-three-products/last-three-pro
   exports: [
     LastThreeProductsComponent,
     CardComponent,
-    CreateCarFormComponent
+    CreateCarFormComponent,
+    EditComponent,
+    DetailsComponent
   ]
 })
 export class ProductModule { }
