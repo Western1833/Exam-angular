454146c8ad06482f67726b0b8d96e3ba843f4366
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-03-13 11:15:09 +0200

    Some fixes and added route to details

--- exam-angular/src/app/app-routing.module.ts
+++ exam-angular/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AllCarsComponent } from './product/all-cars/all-cars.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { CreateCarFormComponent } from './product/create-car-form/create-car-form.component';
+import { DetailsComponent } from './product/details/details.component';
 
 const routes: Routes = [{path: '', component: LastThreeProductsComponent},
   {path: 'catalog', component: AllCarsComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'add-car', component: CreateCarFormComponent}
+  {path: 'add-car', component: CreateCarFormComponent},
+  {path: 'details/:id', component: DetailsComponent},
 ];
 
 @NgModule({
