76db281a036785976bd147f944f6b8cd84d515a3
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-03-19 23:03:22 +0200

    So far i  correctly log email and pass

--- exam-angular/src/app/authentication/authentication.module.ts
+++ exam-angular/src/app/authentication/authentication.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { EmailDirective } from './email.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    EmailDirective
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   exports: [
     LoginComponent,
