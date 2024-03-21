bbb116a8c4cb525b0cfe6d2974c746f66c60b298
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-03-21 18:43:39 +0200

    the start of making the register form reactive

--- exam-angular/src/app/authentication/authentication.module.ts
+++ exam-angular/src/app/authentication/authentication.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { EmailDirective } from './email.directive';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     LoginComponent,
